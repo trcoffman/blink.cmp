8219b58f1c11a2fb1644d3c7116c509fa8348ec0
Liam Dyer <liamcdyer@gmail.com>
2026-09-06 03:40:21 -0400

    feat: add bang/dev for :BlinkCmp build/download

--- plugin/blink-cmp.lua
+++ plugin/blink-cmp.lua
@@ -7,17 +7,29 @@ vim.lsp.config('*', {
 -- Commands
 local subcommands = {
   status = function() vim.cmd('checkhealth blink.cmp') end,
-  build = function() require('blink.cmp.fuzzy.build').build() end,
-  ['build-log'] = function() require('blink.cmp.fuzzy.build').build_log() end,
+  build = function(cmd) require('blink.cmp').build({ force = cmd.bang, dev = cmd.fargs[2] == '--dev' }) end,
+  download = function(cmd) require('blink.cmp').download({ force = cmd.bang }) end,
+  ['build-log'] = function() require('blink.cmp.logger'):open() end,
 }
 vim.api.nvim_create_user_command('BlinkCmp', function(cmd)
   local subcmd_name = cmd.fargs[1]
   local subcmd = subcommands[subcmd_name]
 
-  if type(subcmd) == 'function' then
-    subcmd()
+  if type(subcmd) ~= 'function' then
+    require('blink.cmp.logger'):notify(vim.log.levels.ERROR, "Invalid subcommand '" .. tostring(subcmd_name) .. "'")
+  elseif #cmd.fargs > 1 and (subcmd_name ~= 'build' or #cmd.fargs > 2 or cmd.fargs[2] ~= '--dev') then
+    require('blink.cmp.logger'):notify(vim.log.levels.ERROR, "Invalid arguments for '" .. subcmd_name .. "'")
   else
-    local logger = require('blink.cmp.logger')
-    logger:notify(vim.log.levels.ERROR, "Invalid subcommand '" .. tostring(subcmd_name) .. "'")
+    subcmd(cmd)
   end
-end, { nargs = 1, complete = function() return vim.tbl_keys(subcommands) end, desc = 'blink.cmp' })
+end, {
+  nargs = '+',
+  bang = true,
+  complete = function(_, cmdline, cursorpos)
+    local prefix = cmdline:sub(1, cursorpos)
+    if prefix:match('^%s*BlinkCmp!?%s+%S*$') then return vim.tbl_keys(subcommands) end
+    if prefix:match('^%s*BlinkCmp!?%s+build%s+%S*$') then return { '--dev' } end
+    return {}
+  end,
+  desc = 'blink.cmp',
+})
